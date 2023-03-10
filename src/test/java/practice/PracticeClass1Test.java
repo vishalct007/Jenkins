9be1cf44a62bda0b9b334af4183c5fe1f5f28641
vishalct007 <ctvishal007@gmail.com>
2023-03-10 15:37:37 +0530

    first cmit

---
+++ src/test/java/practice/PracticeClass1Test.java
@@ -0,0 +1,18 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class PracticeClass1Test{
+
+	
+	@Test
+	public void method1() {
+		System.out.println("method1");
+	}
+	@Test
+	public void method2() {
+		System.out.println("method2");
+	}
+	
+
+}
---
+++ src/test/java/practice/PracticeClass2Test.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class PracticeClass2Test{
+	@Test
+	public void method3() {
+		System.out.println("method3");
+	}
+	
+	@Test
+	public void method4() {
+		System.out.println("method4");
+	}
+
+}
